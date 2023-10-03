fce6d242bc495c4d4b814ef1f42242fb05c65c90
kwstn <alex_costi@icloud.com>
2023-10-03 01:54:53 +0100

    Cluedo Card Responses

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ function generateRandomNumber(num)  {
     // Get # (index) from random number between 0 and num - 1
     return Math.floor(Math.random() * num)
 }
+
+const cluedoCards = {
+    characters: ['Mr. Green', 'Miss Peacock', 'Professor Plum', 'Mrs. Scarlett', 'Dr. Orchid'],
+    weapons: ['candlestick', 'wrench', 'lead pipe', 'rope', 'dagger', 'revolver'],
+    rooms: ['hall', 'study', 'ballroom', 'billiards room', 'dining room', 'kitchen', 'lounge', 'conservatory', 'library']
+}
