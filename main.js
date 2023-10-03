dd79d207a3275a99d91342c0b261fe3a250e5f61
kwstn <alex_costi@icloud.com>
2023-10-03 02:05:52 +0100

    Create the answer

--- main.js
+++ main.js
@@ -3,8 +3,30 @@ function generateRandomNumber(num)  {
     return Math.floor(Math.random() * num)
 }
 
-const cluedoCards = {
+const cluedoCards = { // possible cluedo response cards
     characters: ['Mr. Green', 'Miss Peacock', 'Professor Plum', 'Mrs. Scarlett', 'Dr. Orchid'],
+    rooms: ['hall', 'study', 'ballroom', 'billiards room', 'dining room', 'kitchen', 'lounge', 'conservatory', 'library'],
     weapons: ['candlestick', 'wrench', 'lead pipe', 'rope', 'dagger', 'revolver'],
-    rooms: ['hall', 'study', 'ballroom', 'billiards room', 'dining room', 'kitchen', 'lounge', 'conservatory', 'library']
+}
+
+// store answers in an array
+let cluedoAnswer = []
+
+// generate card for each message in the answer
+for (let clue in cluedoCards)   {
+    let index = generateRandomNumber(cluedoCards[clue].length)
+
+    switch(card)    {
+        case 'characters':
+            cluedoAnswer.push(`${cluedoCards[clue][index]} was killed`)
+            break
+        case 'rooms':
+            cluedoAnswer.push(`in the ${cluedoCards[clue][index]}`)
+            break
+        case 'weapons':
+            cluedoAnswer.push(`with the ${cluedoCards[clue][index]}`)
+            break
+        default:
+            cluedoAnswer.push('I think it was you!')
+    }
 }
